2da3764b5ac13c7ebc569ed4e25f4fea7d0b18e5
barry <barry@rika.musicbrainz.org>
2013-06-13 11:12:02 +0000

    Fixed dump script to relativise ultrawrap location

--- dump.sh
+++ dump.sh
@@ -1,2 +1,2 @@
-time java -Xmx10g -jar ultrawrap-compile.jar r2rml -c musicbrainz_compile_config.properties -m $1.ttl -dump $1.nt
+time java -Xmx10g -jar $ULTRAWRAP_HOME/ultrawrap-compile.jar r2rml -c musicbrainz_compile_config.properties -m mappings/$1.ttl -dump output/$1.nt
 #> /dev/null
